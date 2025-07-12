bff7619f79bd4d81d453b8ae454c252fd44ea301
suis-cats <takkunkimidori@gmail.com>
2025-07-13 02:59:23 +0900

    Initial Commit

---
+++ Snap MealTests/Snap_MealTests.swift
@@ -0,0 +1,17 @@
+//
+//  Snap_MealTests.swift
+//  Snap MealTests
+//
+//  Created by Suis on 2025/07/13.
+//
+
+import Testing
+@testable import Snap_Meal
+
+struct Snap_MealTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
